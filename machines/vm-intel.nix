d476b44bec1143bd50caa43cf716e4b62904a04f
Jonathan Rothberg <jon@geneva.com>
2022-04-27 08:41:25 -0700

    Added networking to vm-intel

--- machines/vm-intel.nix
+++ machines/vm-intel.nix
@@ -5,6 +5,9 @@
 
   virtualisation.vmware.guest.enable = true;
 
+  # Interface is this on Intel Fusion
+  networking.interfaces.ens33.useDHCP = true;
+
   # Shared folder to host works on Intel
   fileSystems."/host" = {
     fsType = "fuse./run/current-system/sw/bin/vmhgfs-fuse";
