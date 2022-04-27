6c96a2ae71acee7baf4d5be7677105967d0769a2
Jonathan Rothberg <jon@geneva.com>
2022-04-27 08:40:05 -0700

    Added vm-intel.nix

---
+++ machines/vm-intel.nix
@@ -0,0 +1,21 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports = [ ./vm-shared.nix ];
+
+  virtualisation.vmware.guest.enable = true;
+
+  # Shared folder to host works on Intel
+  fileSystems."/host" = {
+    fsType = "fuse./run/current-system/sw/bin/vmhgfs-fuse";
+    device = ".host/";
+    options = [
+      "umask=22"
+      "uid=1000"
+      "gid=1000"
+      "allow_other"
+      "auto_unmount"
+      "defaults"
+    ];
+  };
+}
