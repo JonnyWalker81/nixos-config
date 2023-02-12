1d1cb753fac6fc06c08c6bc0f079a21223403a24
Jonathan Rothberg <jonathan@join.build>
2023-02-11 16:57:01 -0800

    updated nixos config

--- users/scripts/wttr.sh
+++ users/scripts/wttr.sh
@@ -2,4 +2,4 @@
 
 # curl -s "wttr.in/91106?format=j1" | jq '.current_condition[0].FeelsLikeF' | sed -e 's|["'\'']||g'
 # curl -s "wttr.in/91106?format=j1" | jq --r '.current_condition[0].FeelsLikeF'
-curl -s "wttr.in/91106?format=j1" | jq -r '.current_condition[0] | "\(.FeelsLikeF)\u00B0F, \(.weatherDesc[0].value)"'
+curl -s "wttr.in/91105?format=j1" | jq -r '.current_condition[0] | "\(.FeelsLikeF)\u00B0F, \(.weatherDesc[0].value)"'
