f35aed6b97718f95d2f0c25ff0d5a34add5421d3
Jonathan Rothberg <jon@join.build>
2026-02-25 21:18:11 -0800

    feat(03-01): add baseline daily and weekly planning blocks
    - define block agenda command d with day timeline and unscheduled actionable work
    - define block agenda command w with monday-start week timeline and deadline summary
    - keep planning blocks focused on open TODO/NEXT items with log mode opt-in

--- users/doom.d/config-org-agenda.el
+++ users/doom.d/config-org-agenda.el
@@ -2,11 +2,28 @@
 
 (after! org
   ;; Phase 3 owns agenda command definitions in one place.
+  (setq org-agenda-start-on-weekday 1)
+
   (setq org-agenda-custom-commands
         '(("d" "Daily planning"
-           ((agenda "")))
+           ((agenda ""
+                    ((org-agenda-span 'day)
+                     (org-agenda-overriding-header "Today timeline")))
+            (tags-todo "TODO=\"TODO\"|TODO=\"NEXT\""
+                       ((org-agenda-overriding-header "Unscheduled actionable")
+                        (org-agenda-skip-function '(org-agenda-skip-entry-if 'scheduled 'deadline)))))
+           ((org-agenda-show-log nil)))
           ("w" "Weekly planning"
-           ((agenda ""))))))
+           ((agenda ""
+                    ((org-agenda-span 'week)
+                     (org-agenda-start-on-weekday 1)
+                     (org-agenda-overriding-header "Week timeline")))
+            (tags-todo "DEADLINE<=\"<+7d>\""
+                       ((org-agenda-overriding-header "Weekly deadline summary")))
+            (tags-todo "TODO=\"TODO\"|TODO=\"NEXT\""
+                       ((org-agenda-overriding-header "Unscheduled actionable")
+                        (org-agenda-skip-function '(org-agenda-skip-entry-if 'scheduled 'deadline)))))
+           ((org-agenda-show-log nil))))))
 
 (provide 'config-org-agenda)
 ;;; config-org-agenda.el ends here
