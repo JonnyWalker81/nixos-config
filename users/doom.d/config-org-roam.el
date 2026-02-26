2a4f94a5bbfb609cf9c16a90d0526f87c71f405a
Jonathan Rothberg <jon@join.build>
2026-02-26 15:11:26 -0800

    feat(05-01): add nix-safe org-roam directory and sqlite guard
    - resolve org-roam-directory with file-truename to avoid symlink issues
    - ensure ~/org/roam exists before org-roam startup
    - enable db autosync with explicit sqlite availability enforcement

--- users/doom.d/config-org-roam.el
+++ users/doom.d/config-org-roam.el
@@ -1,4 +1,21 @@
 ;;; config-org-roam.el --- org-roam configuration -*- lexical-binding: t; -*-
 
+(defvar org-life-roam-directory
+  (file-truename (expand-file-name "~/org/roam/"))
+  "Canonical org-roam directory with symlinks resolved.")
+
+(unless (file-directory-p org-life-roam-directory)
+  (make-directory org-life-roam-directory t))
+
+(after! org
+  (setq find-file-visit-truename t)
+  (setq org-roam-directory org-life-roam-directory))
+
+(after! org-roam
+  (unless (and (fboundp 'sqlite-available-p) (sqlite-available-p))
+    (user-error
+     "org-roam requires sqlite support. Rebuild Emacs/Nix config with sqlite enabled before continuing."))
+  (org-roam-db-autosync-mode 1))
+
 (provide 'config-org-roam)
 ;;; config-org-roam.el ends here
