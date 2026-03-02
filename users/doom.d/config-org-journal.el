a301e4028b6eb9bedb138ba95c9e23d9c60fec71
Jonathan Rothberg <jon@join.build>
2026-03-02 11:29:28 -0800

    feat(06-01): configure daily journal scaffold and keypath
    - set org-journal daily file behavior in after! org
    - define lightweight daily header with tasks and reflection prompt
    - bind open-today entrypoint at SPC o j t under org prefix

--- users/doom.d/config-org-journal.el
+++ users/doom.d/config-org-journal.el
@@ -7,5 +7,30 @@
 (unless (file-directory-p org-life-journal-directory)
   (make-directory org-life-journal-directory t))
 
+(defun org-life-journal-open-today ()
+  "Open today's org-journal entry."
+  (interactive)
+  (require 'org-journal)
+  (org-journal-new-entry))
+
+(after! org
+  (setq org-journal-dir org-life-journal-directory)
+  (setq org-journal-file-type 'daily)
+  (setq org-journal-file-format "%Y-%m-%d.org")
+  (setq org-journal-time-format "%H:%M")
+  (setq org-journal-time-prefix "* ")
+  (setq org-journal-file-header
+        (lambda (_time)
+          (concat "#+title: Journal " (format-time-string "%Y-%m-%d (%A)") "\n"
+                  "#+filetags: :journal:\n\n"
+                  "* Tasks\n\n"
+                  "* Journal\n\n"
+                  "* End-of-day Reflection\n"
+                  "- What moved forward today?\n"
+                  "- What is still open?\n")))
+  (map! :leader
+        (:prefix ("o j" . "journal")
+         :desc "Open today's journal" "t" #'org-life-journal-open-today)))
+
 (provide 'config-org-journal)
 ;;; config-org-journal.el ends here
