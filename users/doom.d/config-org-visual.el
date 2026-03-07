5eabf19792446c33356aefacb8f2f543ff7405de
Jonathan Rothberg <jon@join.build>
2026-03-07 15:50:39 -0800

    feat(07-02): add org emphasis reveal and preview automation
    - enable org-appear with contextual marker reveal and anti-flicker delay
    - hide emphasis markers by default in org buffers for cleaner reading
    - auto-render inline images and latex fragments on org buffer open

--- users/doom.d/config-org-visual.el
+++ users/doom.d/config-org-visual.el
@@ -12,6 +12,29 @@
         org-modern-priority-faces nil)
   (add-hook 'org-agenda-finalize-hook #'org-modern-agenda))
 
+(use-package! org-appear
+  :after org
+  :hook (org-mode . org-appear-mode)
+  :config
+  (setq org-appear-autoemphasis t
+        org-appear-autolinks t
+        org-appear-autosubmarkers t
+        org-appear-autoentities t
+        org-appear-delay 0.12
+        org-appear-trigger 'always))
+
+(after! org
+  (setq org-hide-emphasis-markers t
+        org-startup-with-inline-images t
+        org-startup-with-latex-preview t)
+
+  (defun org-life-visual-preview-init-h ()
+    "Render inline images and LaTeX previews when opening Org buffers."
+    (org-display-inline-images)
+    (org-latex-preview '(16)))
+
+  (add-hook 'org-mode-hook #'org-life-visual-preview-init-h))
+
 (after! org-agenda
   (set-face-attribute 'org-agenda-structure nil :weight 'semibold :height 1.0)
   (set-face-attribute 'org-agenda-date nil :weight 'regular)
